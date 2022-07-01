dd5438f5696d75c4bd81987bad0093838c975d51
Barbara-alves <78925296+Barbara-alves@users.noreply.github.com>
2022-06-30 21:05:38 -0300

    exercício vetor - 30.06

---
+++ script.js
@@ -0,0 +1,9 @@
+//01. Crie uma função que recebe um array de 2 números e coloque eles em ordem crescente. Não use o método sort.
+
+let vetor = [1,2];
+
+document.write(vetor[0],'<br>');
+document.write(vetor[1]);
+
+//document.write
+
